f41261a1b9b83aa4448de8d060f8273e9c63abb2
Michael O'Brien <mob@emobrien.com>
2018-02-14 11:45:59 +1100

    REGEN

--- projects/appweb-windows-default-me.h
+++ projects/appweb-windows-default-me.h
@@ -172,7 +172,7 @@
     #define ME_TUNE "size"
 #endif
 #ifndef ME_VERSION
-    #define ME_VERSION "7.0.2"
+    #define ME_VERSION "7.0.3"
 #endif
 #ifndef ME_WATCHDOG_NAME
     #define ME_WATCHDOG_NAME "appman"
@@ -279,10 +279,10 @@
     #define ME_MINOR_VERSION 0
 #endif
 #ifndef ME_PATCH_VERSION
-    #define ME_PATCH_VERSION 2
+    #define ME_PATCH_VERSION 3
 #endif
 #ifndef ME_VNUM
-    #define ME_VNUM 700000002
+    #define ME_VNUM 700000003
 #endif
 
 /* Components */
