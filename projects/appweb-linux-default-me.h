772a45857e12dec33e899774868e65cdb912cfe2
Michael O'Brien <dev@embedthis.com>
2018-11-30 09:20:48 +1100

    REGEN: projects

--- projects/appweb-linux-default-me.h
+++ projects/appweb-linux-default-me.h
@@ -22,7 +22,7 @@
     #define ME_COMPANY "embedthis"
 #endif
 #ifndef ME_COMPATIBLE
-    #define ME_COMPATIBLE "6.2"
+    #define ME_COMPATIBLE "6.3"
 #endif
 #ifndef ME_COMPILER_FORTIFY
     #define ME_COMPILER_FORTIFY 1
@@ -187,7 +187,7 @@
     #define ME_TUNE "size"
 #endif
 #ifndef ME_VERSION
-    #define ME_VERSION "6.2.4"
+    #define ME_VERSION "6.3.0"
 #endif
 #ifndef ME_WATCHDOG_NAME
     #define ME_WATCHDOG_NAME "appman"
@@ -216,7 +216,7 @@
     #define ME_APP_PREFIX "/usr/local/lib/appweb"
 #endif
 #ifndef ME_VAPP_PREFIX
-    #define ME_VAPP_PREFIX "/usr/local/lib/appweb/6.2.4"
+    #define ME_VAPP_PREFIX "/usr/local/lib/appweb/6.3.0"
 #endif
 #ifndef ME_BIN_PREFIX
     #define ME_BIN_PREFIX "/usr/local/bin"
@@ -249,7 +249,7 @@
     #define ME_CACHE_PREFIX "/var/spool/appweb/cache"
 #endif
 #ifndef ME_SRC_PREFIX
-    #define ME_SRC_PREFIX "appweb-6.2.4"
+    #define ME_SRC_PREFIX "appweb-6.3.0"
 #endif
 
 /* Suffixes */
@@ -288,13 +288,13 @@
     #define ME_MAJOR_VERSION 6
 #endif
 #ifndef ME_MINOR_VERSION
-    #define ME_MINOR_VERSION 2
+    #define ME_MINOR_VERSION 3
 #endif
 #ifndef ME_PATCH_VERSION
-    #define ME_PATCH_VERSION 4
+    #define ME_PATCH_VERSION 0
 #endif
 #ifndef ME_VNUM
-    #define ME_VNUM 600020004
+    #define ME_VNUM 600030000
 #endif
 
 /* Components */
