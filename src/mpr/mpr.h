8c9a94b9e3a928e9300cc8224f417a4d0e6f646a
Michael O'Brien <dev@embedthis.com>
2018-12-03 17:20:24 +1100

    REGEN: projects

--- src/mpr/mpr.h
+++ src/mpr/mpr.h
@@ -3090,7 +3090,7 @@ PUBLIC ssize mprPutFmtToWideBuf(MprBuf *buf, cchar *fmt, ...) PRINTF_ATTRIBUTE(2
 /*
     Macros for speed
  */
-#define mprGetBufLength(bp) ((ssize) ((bp)->end - (bp)->start))
+#define mprGetBufLength(bp) ((bp) ? ((ssize) ((bp)->end - (bp)->start)) : 0)
 #define mprGetBufSize(bp) ((bp)->buflen)
 #define mprGetBufSpace(bp) ((bp)->endbuf - (bp)->end)
 #define mprGetBuf(bp) ((bp)->data)
