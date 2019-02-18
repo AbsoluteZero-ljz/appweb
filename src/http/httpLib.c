e0fb27685498587d3304333bf8595eafb3fc354d
Michael O'Brien <dev@embedthis.com>
2019-02-18 14:42:24 +1100

    FIX: lastActivity being set to a stale value

--- src/http/httpLib.c
+++ src/http/httpLib.c
@@ -5682,7 +5682,7 @@ PUBLIC HttpConn *httpCreateConn(HttpEndpoint *endpoint, MprDispatcher *dispatche
     conn->port = -1;
     conn->retries = HTTP_RETRIES;
     conn->endpoint = endpoint;
-    conn->lastActivity = HTTP->now;
+    conn->lastActivity = HTTP->now = mprGetTicks();
     conn->ioCallback = httpIOEvent;
 
     if (endpoint) {
