f63366d37d571cb8878ed4a52abbf2de079740c7
Michael O'Brien <dev@embedthis.com>
2019-04-23 18:47:40 +1000

    DOC: add client samples

--- samples/threaded-client/app-xcode/Debug/client.build/Objects-normal/x86_64/client.d
+++
@@ -1,6 +0,0 @@
-dependencies: /Users/mob/dev/appweb-core/samples/threaded-client/client.c \
-  /Users/mob/dev/appweb-core/build/macosx-x64-debug/inc/appweb.h \
-  ../../build/macosx-x64-debug/inc/osdep.h \
-  ../../build/macosx-x64-debug/inc/me.h \
-  ../../build/macosx-x64-debug/inc/mpr.h \
-  ../../build/macosx-x64-debug/inc/http.h
