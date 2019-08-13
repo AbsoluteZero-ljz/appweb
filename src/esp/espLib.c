b470121765717da40b14b98e6c887fcc77520028
Michael O'Brien <dev@embedthis.com>
2019-08-13 13:00:45 +1000

    FIX: remove unwanted MprModule arg to ESP

--- src/esp/espLib.c
+++ src/esp/espLib.c
@@ -6602,7 +6602,7 @@ PUBLIC char *espBuildScript(HttpRoute *route, cchar *page, cchar *path, cchar *c
             "static void %s(HttpConn *conn) {\n"\
             "%s%s%s"\
             "}\n\n"\
-            "%s int esp_%s(HttpRoute *route, MprModule *module) {\n"\
+            "%s int esp_%s(HttpRoute *route) {\n"\
             "   espDefineView(route, \"%s\", %s);\n"\
             "   return 0;\n"\
             "}\n",
