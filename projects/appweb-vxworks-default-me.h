2a52748274e0f8b28101190e8fed96304d355f4f
Michael O'Brien <mob@emobrien.com>
2020-08-31 08:34:20 +1000

    DEV: regen projects doc

--- projects/appweb-vxworks-default-me.h
+++ projects/appweb-vxworks-default-me.h
@@ -202,7 +202,7 @@
     #define ME_TUNE "size"
 #endif
 #ifndef ME_VERSION
-    #define ME_VERSION "8.1.0"
+    #define ME_VERSION "8.1.1"
 #endif
 #ifndef ME_WATCHDOG_NAME
     #define ME_WATCHDOG_NAME "appman"
@@ -264,7 +264,7 @@
     #define ME_VAPP_PREFIX "deploy"
 #endif
 #ifndef ME_SRC_PREFIX
-    #define ME_SRC_PREFIX "/usr/src/appweb-8.1.0"
+    #define ME_SRC_PREFIX "/usr/src/appweb-8.1.1"
 #endif
 
 /* Suffixes */
@@ -306,10 +306,10 @@
     #define ME_MINOR_VERSION 1
 #endif
 #ifndef ME_PATCH_VERSION
-    #define ME_PATCH_VERSION 0
+    #define ME_PATCH_VERSION 1
 #endif
 #ifndef ME_VNUM
-    #define ME_VNUM 800010000
+    #define ME_VNUM 800010001
 #endif
 
 /* Components */
